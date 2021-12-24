ce158f03b02fc0245620d752725b6b3e840704d8
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-12-24 13:51:35 +0700

    push

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -18,13 +18,69 @@ import (
 // * Peeking form an empty stack return an error: ErrNoSuchElement
 
 func TestNewSet(t *testing.T) {
+	s := stack.New()
 	t.Run("A Stack is empty on construction", func(t *testing.T) {
-		s := stack.New()
 		assert.True(t, s.IsEmpty())
 	})
 
 	t.Run("A Stack has size 0 on construction", func(t *testing.T) {
-		s := stack.New()
+
 		assert.Equal(t, 0, s.Size())
+	})
+}
+
+func TestInsert(t *testing.T) {
+	t.Run("After n pushes to an empty stack (n>0), the stack is non-empty and its size equals n", func(t *testing.T) {
+		s := stack.New()
+		s.Push(1)
+		s.Push(2)
+		s.Push(3)
+		assert.False(t, s.IsEmpty())
+		assert.Equal(t, 3, s.Size())
+	})
+
+	t.Run("If one pushes x then pops, the value pooped is x, and the size is one less than old size", func(t *testing.T) {
+		s := stack.New()
+		s.Push(1)
+		s.Push(2)
+		s.Push(6)
+		assert.Equal(t, 3, s.Size())
+		val, _ := s.Pop()
+		assert.Equal(t, 6, val)
+		assert.Equal(t, 2, s.Size())
+
+	})
+
+	t.Run("If one pushes x then pops, the value pooped is x, and the size is one less than old size", func(t *testing.T) {
+		s := stack.New()
+		s.Push(1)
+		s.Push(2)
+		s.Push(6)
+		assert.Equal(t, 3, s.Size())
+		val, _ := s.Peek()
+		assert.Equal(t, 6, val)
+		assert.Equal(t, 3, s.Size())
+	})
+}
+
+func TestError(t *testing.T) {
+	t.Run("Popping form an empty stack return an error: ErrNoSuchElement", func(t *testing.T) {
+		s := stack.New()
+		_, err := s.Pop()
+		if err == nil {
+			t.Fail()
+			t.Logf("Expres error is not nil, but got '%v'", err)
+		}
+		assert.Equal(t, stack.ErrNoSuchElement, err)
+	})
+
+	t.Run("Peeking form an empty stack return an error: ErrNoSuchElement", func(t *testing.T) {
+		s := stack.New()
+		_, err := s.Peek()
+		if err == nil {
+			t.Fail()
+			t.Logf("Expres error is not nil, but got '%v'", err)
+		}
+		assert.Equal(t, stack.ErrNoSuchElement, err)
 	})
 }
